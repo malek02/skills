8d261e78be5887f538cbd530df771b28a7a0d93c
Malek Mechi <bouallagui@sogeclairis.fr>
2020-08-24 17:03:55 +0100

    second

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongoose');
+const config = require('config');
+const db = config.get('URI');
+
+
+const connectDB = async ()=> {
+    try{
+        await mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true })
+        console.log("work goood......");
+    }
+    catch(err){
+console.error(err.message);
+process.exit(1)
+    }
+}
+
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
-const PORT = process.env.PORT || 4000;
-app.post('/',(req,res)=>res.send('HELOO FROM FFFSERVER'))
 
 
+
+app.use(express.json({extended: false}));
+
+const PORT = process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    console.log('ffff',req.body);
+    res.send('HELOO FROM FFFSERVER')})
+
+app.use('/api/users', require('./router/api/users'));
+app.use('/api/auth', require('./router/api/auth'));
+app.use('/api/profile', require('./router/api/profile'));
+app.use('/api/posts', require('./router/api/posts'));
 
 
 
